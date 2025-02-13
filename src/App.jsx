c92e1daca7b7214431685db885cfd6ea9ef3e280
sudh4800@gmail.com <sudh4800@gmail.com>
2025-02-14 02:42:36 +0530

    orderLoader and createOrderAction

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Home from "./UI/Home"
 import Error from "./UI/Error"
 import Menu, { loader as menuLoader } from "./features/menu/Menu"
 import Cart from "./features/cart/Cart"
-import CreateOrder from "./features/order/CreateOrder"
-import Order from "./features/order/Order"
+import CreateOrder, { action as createOrderAction } from "./features/order/CreateOrder"
+import Order, { loader as orderLoader } from "./features/order/Order"
 import AppLayout from "./UI/AppLayout"
 
 const router = createBrowserRouter([
@@ -21,6 +21,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
@@ -28,11 +29,15 @@ const router = createBrowserRouter([
       },
       {
         path: "/order/new",
-        element: <CreateOrder />
+        element: <CreateOrder />,
+        action: createOrderAction
       },
       {
         path: "/order/:orderId",
-        element: <Order />
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+
       },
     ]
 
--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 export default function Header() {
   return (
     <header>
       <Link to='/'>The Fast Pizza Co.</Link>
+      <SearchOrder />
       <p>Sudhanshu Singh</p>
     </header>
   )
