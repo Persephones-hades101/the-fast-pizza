4fd3bbe20988b5eb35c36d98590028b478c238e0
sudh4800@gmail.com <sudh4800@gmail.com>
2025-02-13 19:32:40 +0530

    initial setup and AppLayout

---
+++ src/UI/AppLayout.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom"
+
+import Header from "./Header"
+import CartOverview from "../features/cart/CartOverview"
+
+
+export default function AppLayout() {
+  return (
+    <div>
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <CartOverview />
+
+    </div>
+  )
+}
---
+++ src/UI/Header.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header>
+      <Link to='/'>The Fast Pizza Co.</Link>
+      <p>Sudhanshu Singh</p>
+    </header>
+  )
+}
