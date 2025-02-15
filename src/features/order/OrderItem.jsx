60434b949765316887f5c6a80a5538653b72c154
sudh4800@gmail.com <sudh4800@gmail.com>
2025-02-16 00:11:03 +0530

    showing ingredients in orderitem component by fetching menu data without even navigating to menu component

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -4,13 +4,16 @@ function OrderItem({ item, isLoadingIngredients, ingredients }) {
   const { quantity, name, totalPrice } = item;
 
   return (
-    <li className="py-3">
+    <li className="space-y-1 py-3">
       <div className="flex flex-wrap items-center justify-between gap-2 text-sm">
         <p>
           <span className="font-bold">{quantity}&times;</span> {name}
         </p>
         <p className="font-bold">{formatCurrency(totalPrice)}</p>
       </div>
+      <p className="text-xs capitalize italic text-stone-500 md:text-sm">
+        {isLoadingIngredients ? "Loading..." : ingredients.join(", ")}
+      </p>
     </li>
   );
 }
